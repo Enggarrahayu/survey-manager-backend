297a96d90573457756d1324ba1f315755ebc3a24
Enggar Rahayu <enggarrahayu64@gmail.com>
2021-07-01 11:07:54 +0700

    add database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+        	'username' => 'enggarrahayu',
+        	'email' => 'enggarrahayu@gmail.com',
+        	'password' => 'enggarrahayu'
+        ]);
     }
 }
